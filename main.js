0d18547b4ef403b30557fb46c2e752157725dc97
Nguyen Duy Manh <manhnd@evolable.asia>
2018-07-06 16:58:29 +0900

    fix main js

--- main.js
+++ main.js
@@ -60,10 +60,6 @@ function playStream(idVideoTag, stream){
 	video.play();
 }
 
-//openStream()
-//	.then(stream => playStream('localStream', stream));
-	
-//test-peerjs-eas.herokuapp.com
 
 //ket noi server peerjs
 const peer = new Peer({
@@ -87,41 +83,27 @@ peer.on('open', id => {
 		console.log(username);
 		socket.emit('NGUOI_DUNG_DANG_KY', {ten: username, peerId: id});
 	});
-    //$('#btnSignUp').click(() => {
-    //    const username = $('#txtUsername').val();
-    //    socket.emit('NGUOI_DUNG_DANG_KY', {
-    //        ten: username,
-    //        peerId: id
-    //    });
-    //});
 });
 
-//caller
-$("#btnCall").click(()=>{
-	const id = $("#call_peer").val();
-	console.log("call to :" +id);
-	openStream()
-	.then(stream => {
-		playStream("localStream", stream);
-		var call = peer.call(id, stream);
-		call.on("stream", remoteStream=> playStream("remoteStream",stream));
-	});
+//Caller
+$('#btnCall').click(() => {
+    const id = $('#remoteId').val();
+    openStream()
+        .then(stream => {
+            playStream('localStream', stream);
+            const call = peer.call(id, stream);
+            call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+        });
 });
 
-//get call
-peer.on("call", call => {
-	console.log("Da nhan duoc call");
-	console.log(call);
-	call.answer(stream); //Tra loi da nhan call
-		//playStream("localStream", stream);
-	call.on("stream", remoteStream => playStream("remoteStream",remoteStream));
-	//openStream()
-	//.then(stream => {
-	//	call.answer(stream); //Tra loi da nhan call
-	//	//playStream("localStream", stream);
-	//	call.on("stream", remoteStream => playStream("remoteStream",remoteStream));
-		
-	//});
+//Callee
+peer.on('call', call => {
+    openStream()
+        .then(stream => {
+            call.answer(stream);
+            playStream('localStream', stream);
+            call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+        });
 });
 
 
